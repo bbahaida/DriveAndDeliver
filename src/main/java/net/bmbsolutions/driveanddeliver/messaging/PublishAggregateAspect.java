964620bbe2bb3044d15e93aa1e73f45b03256918
Brahim Bahaida <bbahieda@gmail.com>
2024-07-08 13:40:40 +0200

    fix: condition kafka

--- src/main/java/net/bmbsolutions/driveanddeliver/messaging/PublishAggregateAspect.java
+++ src/main/java/net/bmbsolutions/driveanddeliver/messaging/PublishAggregateAspect.java
@@ -3,6 +3,7 @@ package net.bmbsolutions.driveanddeliver.messaging;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +17,12 @@ public class PublishAggregateAspect {
     public PublishAggregateAspect(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
-
+    @Value("kafka.broker-url")
+    private String kafkaUrl;
     @AfterReturning("execution(* org.springframework.data.jpa.repository.JpaRepository+.save(..)) && args(entity)")
     public void publishAggregateAfterSave(Object entity) {
         PublishAggregate annotation = entity.getClass().getAnnotation(PublishAggregate.class);
-        if (annotation != null) {
+        if (annotation != null && kafkaUrl != null) {
             String topic = annotation.topic();
             kafkaTemplate.send(topic, entity);
         }
